f1968173434f46b2798abbb51589333a1a7cf9ad
Ignacio Ceaglio <ignacioceaglio@gmail.com>
2021-03-24 22:03:30 -0300

    Added Basic Video Sharing features

--- src/contracts/DVideo.sol
+++ src/contracts/DVideo.sol
@@ -1,32 +1,57 @@
 pragma solidity ^0.5.0;
 
+
+/**+-Steps for the Video being available in the Platform:_
++-(1)-Model the Video, giving it a Data Structure.
++-(2)-Store the Video.
++-(3)-Upload Video.
++-(4)-List Video.*/
+
 contract DVideo {
   uint public videoCount = 0;
   string public name = "DVideo";
-  //Create id=>struct mapping
+  /**+-(2)-Store the Video.*/
+  /**+-(4)-List Video.*/
+  //+-Create id=>struct mapping:_
+  mapping(uint => Video) public videos;
 
-  //Create Struct
+  /**+-(1)-Model the Video.*/
+  //+-Create Struct:_
+  struct Video {
+    uint id;
+    string hash;
+    string title;
+    address author;
+  }
 
-
-  //Create Event
+  //Create a "Video Uploaded" Event Listener.
+  event VideoUploaded(
+    uint id,
+    string hash,
+    string title,
+    address author
+  );
 
 
   constructor() public {
   }
 
-  function uploadVideo(string memory _videoHash, string memory _title) public {
-    // Make sure the video hash exists
+  /**+-(3)-Upload Video.*/
+  function uploadVideo(string memory _videoHash, string memory _title) public {/**(We use "memory" in the Arguments of this Func. because the Uploading process is temporary).*/
+    //+-Make sure the video hash exists:_
 
-    // Make sure video title exists
+    //+-Make sure video title exists:_
 
-    // Make sure uploader address exists
+    //+-Make sure uploader address exists:_
 
 
-    // Increment video id
+    //+-Increment video id:_
+    videoCount ++;
 
-    // Add video to the contract
+    //+-Add video to the contract:_
+    videos[videoCount] = Video(videoCount, _videoHash, _title, msg.sender);
 
-    // Trigger an event
-
+    //+-Trigger the "Video Uploaded" Event Listener.:_
+    emit VideoUploaded(videoCount, _videoHash, _title, msg.sender);
   }
 }
